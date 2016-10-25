c1e5bb5e75cc570ac220106bb0a2bc4e0b6a40b7
Carlos <carlos@caap.localdomain>
2016-10-25 07:57:08 -0500

    OK

--- test.lua
+++ test.lua
@@ -1,34 +1,28 @@
 #!/usr/local/bin/lua
 
 local sql = require'carlos.sqlite'
----[[
+
+local conn = sql.connect'/db/inventario.db'
+
+assert(conn.exec'DROP VIEW IF EXISTS compras')
+assert(conn.exec'CREATE VIEW IF NOT EXISTS stock AS SELECT faltantes.clave, obs, proveedor FROM proveedores, faltantes WHERE proveedores.clave = faltantes.clave')
+
+conn = sql.connect'/db/ferre.db'
+
+assert(conn.exec'DROP VIEW IF EXISTS precios')
+assert(conn.exec'CREATE VIEW IF NOT EXISTS precios AS SELECT clave, desc, fecha, u1, ROUND(prc1*costol/1e4,2) precio1, u2, ROUND(prc2*costol/1e4,2) precio2, u3, ROUND(prc3*costol/1e4,2) precio3, costol FROM datos')
+
+--[[
 local conn = sql.connect'/db/inventario.db'
 
 assert(conn.exec'ATTACH DATABASE "/db/ferre.db" AS FR')
 
 assert(conn.exec(string.format(sql.newTable, 'faltantes', 'clave PRIMARY KEY, faltante INTEGER DEFAULT 0, obs DEFAULT ""')))
 assert(conn.exec'INSERT INTO faltantes SELECT clave, faltante, obs FROM FR.faltantes')
-assert(conn.exec'CREATE VIEW IF NOT EXISTS compras AS SELECT faltantes.clave, obs, proveedor FROM proveedores, faltantes WHERE faltante = 1 AND proveedores.clave = faltantes.clave')
-
 conn = sql.connect'/db/ferre.db'
 assert(conn.exec'DROP TABLE faltantes')
 assert(conn.exec'DROP TABLE categorias')
 assert(conn.exec'DROP TABLE cambios')
 assert(conn.exec'DROP TABLE ubicacion')
---]]
---[[
-conn = sql.connect'/db/updates.db'
-assert(conn.exec(string.format(sql.newTable,'cambios', 'clave, campo, version INTEGER, PRIMARY KEY(clave, campo)')))
-
-assert(conn.exec'ATTACH DATABASE "/db/ferre.db" AS FR')
-
-assert(conn.exec'CREATE TABLE claves AS SELECT clave FROM datos')
-
-for _,v in ipairs({'fecha', 'desc', 'u1', 'u2', 'u3', 'costol', 'faltante', 'obs', 'proveedor'}) do
-assert(conn.exec(string.format('INSERT INTO cambios SELECT clave, %q, 1 FROM claves', v)))
-assert(conn.exec(string.format("INSERT INTO cambios VALUES ('VERS', %q, 1)", v)))
-end
-
-assert(conn.exec'DROP TABLE claves')
 --]]
 
