8230c0b31b86acebe0ea8d463507411451e0ede1
Charlie Root <root@ferreb.rga.ip>
2017-03-14 17:15:52 -0600

    OK

--- test.lua
+++ test.lua
@@ -4,14 +4,24 @@ local sql = require'carlos.sqlite'
 
 local conn = sql.connect'/db/inventario.db'
 
+--[[
 assert(conn.exec'DROP VIEW IF EXISTS compras')
 assert(conn.exec'CREATE VIEW IF NOT EXISTS stock AS SELECT faltantes.clave, faltante, obs, proveedor FROM proveedores, faltantes WHERE proveedores.clave = faltantes.clave')
 
 assert(conn.exec'UPDATE proveedores SET proveedor = UPPER(proveedor)')
 assert(conn.exec'UPDATE proveedores SET proveedor = "X" WHERE proveedor LIKE " "')
+--]]
 
 conn = sql.connect'/db/ferre.db'
 
+assert(conn.exec'CREATE TABLE IF NOT EXISTS tags ( id INTEGER PRIMARY KEY, nombre )')
+assert(conn.exec"INSERT INTO tags VALUES (97, 'presupuesto')")
+assert(conn.exec"INSERT INTO tags VALUES (98, 'ticket')")
+assert(conn.exec"INSERT INTO tags VALUES (99, 'facturar')")
+assert(conn.exec"INSERT INTO tags VALUES (103, 'guardar')")
+
+
+--[[
 assert(conn.exec'DROP VIEW IF EXISTS precios')
 assert(conn.exec'CREATE VIEW IF NOT EXISTS precios AS SELECT clave, desc, fecha, u1, ROUND(prc1*costol/1e4,2) precio1, u2, ROUND(prc2*costol/1e4,2) precio2, u3, ROUND(prc3*costol/1e4,2) precio3, costol FROM datos')
 
